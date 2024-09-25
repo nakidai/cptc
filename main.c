0005e1416da92aeb1c7fa0c1617020be001b7bf4
Nakidai <nakidai@disroot.org>
2024-09-26 00:17:34 +0300

    Add importing token from env

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "cptc.h"
 
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 
+const char *CPTC_token;
+
 int main(void)
 {
+    CPTC_token = getenv("CPTC_TOKEN");
     CPTC("127.0.0.1", 8080);
 }
